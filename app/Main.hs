e40edaa6d557c6a13c0869c506c2901f142f72a6
Alexandre Krispin <k.m.alexandre@gmail.com>
2019-05-01 01:45:54 +0900

    Add web service to serve json

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,57 @@
+#!/usr/bin/env stack
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes #-}
+-- {-# LANGUAGE ScopedTypeVariable #-}
 module Main where
 
 import qualified Process.Information as PI
 import qualified Process.Manage as PM
 import qualified Process.Monitor as PMO
 import qualified Process.Resources as R
+import Data.Semigroup ((<>))
+import Options.Applicative
+import Process.Web.Server
+
+-- start server
+-- create process
+-- show cpu usage of a process
+-- show all processes with list all
+data Options = Options
+  { optCommand :: !Server
+  }
+
+data Manage = Create String String | Stop String
+data Info = Find String
+data Server = Start String | Stop
 
 
+main :: IO ()
 main = do
-  -- P.start "kate"
-  PMO.create "thunderbird" "thunderbird"
-  cpuUsage <- R.cpuUsage
-  putStrLn $ show cpuUsage
-  kate <- PM.findProcess PM.PName "thunderbird"
-  putStrLn $ show kate
-  environ <- PI.listProcessEnviron "3654"
-  putStrLn $ show environ
-  cwd <- PI.seeCwd "3654"
-  putStrLn cwd
+  (opts :: Options) <- execParser optsParser
+  case optCommand opts of
+    Start ipAddr -> start ipAddr
+  where
+    optsParser :: ParserInfo Options
+    optsParser =
+      info
+        (fullDesc <> progDesc "Command line application to parse processes" <>
+         header
+           "Targeted for Linux, BSD and Windows platforms")
 
-  -- result <- P.kill $ kate !! 0
-  -- putStrLn $ show result
-  -- p <- processes
-  -- print p
+
+
+-- main = do
+--   -- P.start "kate"
+--   PMO.create "thunderbird" "thunderbird"
+--   cpuUsage <- R.cpuUsage
+--   putStrLn $ show cpuUsage
+--   kate <- PM.findProcess PM.PName "thunderbird"
+--   putStrLn $ show kate
+--   environ <- PI.listProcessEnviron "3654"
+--   putStrLn $ show environ
+--   cwd <- PI.seeCwd "3654"
+--   putStrLn cwd
+
+--   -- result <- P.kill $ kate !! 0
+--   -- putStrLn $ show result
+--   -- p <- processes
+--   -- print p
--- src/Process/Web/Server.hs
+++ src/Process/Web/Server.hs
@@ -2,8 +2,17 @@
 module Process.Web.Server where
 
 import Web.Scotty
+import Data.Default.Class (def)
 import Data.Monoid (mconcat)
+import qualified Network.Wai.Handler.Warp as W
 
-main = scotty 3000 $
-  get ":/processes" $ do
-  html $ mconcat ["<h1>Scotty, beam me up!</h1>"]
+
+-- customSettings :: String -> Option
+customSettings ipAddr = def { verbose = 1
+                     , settings = W.setHost ipAddr $ settings def
+                     }
+
+
+start :: IO ()
+start ipAddr = scottyOpts $ customSettings ipAddr $ do
+  get "/" $ text "hello world"
