9a83f184d3022d9d4f1c7f8fd729f234c9d080a4
Alexandre Krispin <k.m.alexandre@gmail.com>
2019-05-04 13:21:35 +0900

    Add skeleton for an HTML dashboard

--- src/Process/Web/Internal/Index.hs
+++ src/Process/Web/Internal/Index.hs
@@ -1,15 +1,83 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Process.Web.Internal.Index where
 
-import Text.Blaze as TB
-import Text.Blaze.Html5 as H
-import Text.Blaze.Html5.Attributes as HA
-import Text.Blaze.Html.Renderer.Text as HRT
-import Text.Blaze.Html.Renderer.String as HRS
-import Text.Blaze.Internal as BI
+import Prelude hiding (div, head, id, span)
+import Text.Blaze.Html5 (
+  (!),
+  Html,
+  a,
+  body,
+  div,
+  docTypeHtml,
+  footer,
+  h1,
+  h3,
+  head,
+  li,
+  link,
+  nav,
+  p,
+  script,
+  span,
+  title,
+  ul)
+import Text.Blaze.Html5.Attributes (
+  class_,
+  href,
+  id,
+  rel,
+  src)
+-- import Text.Blaze.Html.Renderer.Text as HRT
+-- import Text.Blaze.Html.Renderer.String as HRS
+-- import Text.Blaze.Internal as BI
 
-index :: H.Html
+index :: Html
 index = do
-  H.docTypeHtml $ do
-    H.head $ do
-      H.title "ProcPS dashboard"
+  docTypeHtml $ do
+    head $ do
+      link ! rel "stylesheet" ! href "https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+      link ! rel "stylesheet" ! href "https://fonts.googleapis.com/icon?family=Material+Icons"
+      link ! rel "stylesheet" ! href "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
+      script ! src "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js" $ mempty
+      title "ProcPS dashboard"
+    body $ do
+      page
+
+
+page :: Html
+page = do
+  div ! class_ "row" $ do
+    div ! class_ "col s12 m12" $ do
+        header'
+        dashboard
+        footer'
+
+
+dashboard :: Html
+dashboard = do
+  div ! class_ "row" $ do
+    div ! class_ "col s6 m6" $ do
+      div ! class_ "card blue-grey darken-1" $ do
+        div ! class_ "card-content white-text" $ do
+          span ! class_ "card-title" $ "Firefox"
+          p "Process Information"
+        div ! class_ "card-action" $ do
+          a ! href "#" $ "restart process"
+          a ! href "#" $ "stop process"
+
+
+header' :: Html
+header' = do
+  nav $ do
+    div ! class_ "brand-logo" $ "ProcPS Dashboard"
+    -- ul ! id "nav-mobile" ! class_ "right hide-on-med-and-down" $ do
+    --   li $ a ! href "#" $ "Home"
+
+
+footer' :: Html
+footer' = do
+  footer ! class_ "page-footer" $ do
+    div ! class_ "container" $ do
+      div ! class_ "row" $ do
+        div ! class_ "col 16 s12" $ do
+          h3 ! class_ "white-text" $ "footer"
